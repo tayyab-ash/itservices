acbd6c4c6fff3c8aecf719f830570d7c0b4751fd
Talha <mtalha7274@gmail.com>
2024-06-09 19:03:29 +0500

    deployed on firebase

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import Landingpage from "./Components/Landingpage/Landingpage";
 import AppState from "./Components/Context/AppState";
 import React from "react";
 
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Landingpage/Navbar";
 import Footer from "./Components/Landingpage/Footer";
 import ServicesPage from "./Components/ServicesPage/ServicesPage";
@@ -15,35 +11,34 @@ import AboutUsPage from "./Components/AboutUsPage/AboutUsPage";
 import Testimonals from "./Components/Landingpage/Testimonals";
 import ContactSection from "./Components/Landingpage/ContactSection";
 
-
-
-
-
 function App() {
   return (
     <>
       <AppState>
         <Router>
           <Routes>
-
-          <Route
-            path="/*"
-            element={
-              <React.Fragment>
-                <Navbar/>
-                <div>
-                  <Routes>
-                    <Route exact path="/" element={<Landingpage />} />
-                    <Route exact path="/services"  element={<ServicesPage/>} />
-                    <Route exact path="/aboutus"  element={<AboutUsPage/>} />
-                  </Routes>
-                </div>
-                <Testimonals/>
-                <ContactSection/>
-                <Footer/>
-              </React.Fragment>
-            }
-          />
+            <Route
+              path="/*"
+              element={
+                <React.Fragment>
+                  <Navbar />
+                  <div>
+                    <Routes>
+                      <Route exact path="/" element={<Landingpage />} />
+                      <Route
+                        exact
+                        path="/services"
+                        element={<ServicesPage />}
+                      />
+                      <Route exact path="/aboutus" element={<AboutUsPage />} />
+                    </Routes>
+                  </div>
+                  <Testimonals />
+                  <ContactSection />
+                  <Footer />
+                </React.Fragment>
+              }
+            />
           </Routes>
         </Router>
       </AppState>
